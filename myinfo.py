b9328ac6818ba1a76f6d8049021e8d34101c5253
itsmadelineyo <madison.dorse@gmail.com>
2025-02-15 16:00:28 -0600

    Create myinfo.py

---
+++ myinfo.py
@@ -0,0 +1,7 @@
+>>> print("Madison Dorse")
+Madison Dorse
+>>> print("My address because I don't feel comfortable writing it out (:")
+My address because I don't feel comfortable writing it out (:
+>>> print("My phone number")
+My phone number
+>>>
